8faafaa1383083365c1f806aea5602f32e32fb95
Ryan Whelchel <rydwhelchel@gmail.com>
2021-11-15 15:24:23 -0500

    Quick patch for accidentally modified file.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import EventList from './components/EventList';
 
 const App = function App() {
-  // const args = JSON.parse(document.getElementById("data").text);
+  const args = JSON.parse(document.getElementById('data').text);
   // Comment the above when using a local react app,
   // comment the below args when using Flask to run on npm start
 
-  const args = {
-    events: [
-      { title: 'Meeting with Prof', date: '2021-11-11T12:00' },
-      { title: 'Anniversary', date: '2021-11-09' },
-      { title: "Mom's Birthday", date: '2021-11-10T12:00' },
-      { title: 'Assignment Due', date: '2021-11-12T23:59' },
-      { title: 'Test!', date: '2021-11-09T12:00' },
-      { title: 'Go out with friends', date: '2021-11-20T18:00' },
-      { title: 'My Birthday!', date: '2021-01-16' },
-      { title: 'My Birthday next year!', date: '2021-11-25' },
-      { title: 'Thanksgiving', date: '2022-01-16' },
-    ],
-  };
+  // const args = {
+  //   events: [
+  //     { title: 'Meeting with Prof', date: '2021-11-11T12:00' },
+  //     { title: 'Anniversary', date: '2021-11-09' },
+  //     { title: "Mom's Birthday", date: '2021-11-10T12:00' },
+  //     { title: 'Assignment Due', date: '2021-11-12T23:59' },
+  //     { title: 'Test!', date: '2021-11-09T12:00' },
+  //     { title: 'Go out with friends', date: '2021-11-20T18:00' },
+  //     { title: 'My Birthday!', date: '2021-01-16' },
+  //     { title: 'My Birthday next year!', date: '2021-11-25' },
+  //     { title: 'Thanksgiving', date: '2022-01-16' },
+  //   ],
+  // };
 
   return <EventList events={args.events} />;
 };
