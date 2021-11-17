fd66fb9712f2a4225d84ad94d275fe4a5f017d47
Linlin Lu <pku600@126.com>
2021-11-16 22:35:42 -0500

    addHelperFunc

--- resources/helper.py
+++ resources/helper.py
@@ -14,3 +14,20 @@ def get_dates_titles(requested_data):
         event_titles.append(i["title"])
         event_dates.append(i["date"])
     return event_dates, event_titles
+
+
+def to_delete_events(existing_titles, events):
+    return [
+        (title, event)
+        for title, event in zip(existing_titles, events)
+        if title not in event_titles
+    ]
+
+
+def to_add_events(event_titles, event_dates):
+    return [
+        (title, date)
+        for title, date in zip(event_titles, event_dates)
+        if title not in existing_titles
+    ]
+
