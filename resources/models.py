16c34e5e8937360ee14148bfbe0e12cfd6f58f93
Linlin Lu <pku600@126.com>
2021-11-21 21:15:24 -0500

    EventText model

--- resources/models.py
+++ resources/models.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy(app)
 
+
 class Person(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -20,14 +21,27 @@ class Person(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 class Event(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(64))
-    username=db.Column(db.String(64))
-    date=db.Column(db.String(64))
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    username = db.Column(db.String(64))
+    date = db.Column(db.String(64))
     # folder=db.Column(db.String(64))
 
     def __repr__(self):
         return "<Title {}>, Date {}".format(self.title, self.date)
+
+
+class EventText(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    username = db.Column(db.String(64))
+    text = db.Column(db.String(8000))
+    # folder=db.Column(db.String(64))
+
+    def __repr__(self):
+        return "<Title {}>, Text {}".format(self.title, self.text)
+
 
 db.create_all()
