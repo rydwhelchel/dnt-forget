4d4097dda3e65067da0e1366ef7f27d31e25b1b0
Ryan Whelchel <rydwhelchel@gmail.com>
2021-11-28 18:54:16 -0500

    Beautified alerts, added save-alert

--- src/index.js
+++ src/index.js
@@ -10,9 +10,15 @@ import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 
 const rootElement = document.getElementById('root');
+const options = {
+  position: 'bottom center',
+  timeout: 5000,
+  offset: '30px',
+  transition: 'scale',
+};
 ReactDOM.render(
   <BrowserRouter>
-    <AlertProvider template={AlertTemplate}>
+    <AlertProvider template={AlertTemplate} {...options}>
       <App />
     </AlertProvider>
   </BrowserRouter>,
